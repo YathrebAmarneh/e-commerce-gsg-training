6a269ecb643db7e8de6fa71efceab23774e39ab9
Yathreb <yathrebamarneh93@gmail.com>
2022-11-28 13:40:22 +0200

    login styling

--- src/components/navbar/navigation-links/index.js
+++ src/components/navbar/navigation-links/index.js
@@ -3,7 +3,7 @@ import style from "./style.module.css";
 import { useNavigate } from "react-router-dom";
 
 const NavigationLinks = () => {
-  const data = ["HOME", "PRODUCTLISTING", "JOURNAL", "ABOUT"];
+  const data = ["HOME", "PRODUCTLISTING"];
   const navigate = useNavigate();
 
   return (
@@ -11,7 +11,7 @@ const NavigationLinks = () => {
       {data.map((item, index) => {
         return (
           <>
-            <a key={index} href={`#${item.toLowerCase()}`} onClick={() => navigate(`/${'HOME' ? '' : item}`)}>
+            <a key={index} onClick={() => navigate(`/${item==='HOME'?'':item}`)}>
               {item}
               <IoIosArrowDown className="IoIosArrowDown" />
             </a>
